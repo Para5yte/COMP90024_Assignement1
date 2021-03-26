935d23c4e0fb720e24043e7e8ab43053c920917b
para5yte <tmitsu195@gmail.com>
2021-03-27 00:14:41 +1100

    changed

--- main.py
+++ main.py
@@ -216,7 +216,7 @@ def main():
             # print("number of tweets in", cell, cells[cell].num_tweet)
 
             # print("process 0 receives message from process", proc_id, ":", message)
-        with open("Output.txt", "w") as text_file:
+        with open("Output.txt", "a+") as text_file:
             print("process", my_rank, "number of tweets this process went through", number_of_tweets, file=text_file)
             print("--- %s seconds ---" % (time.time() - start_time), file=text_file)
         #print("--- %s seconds ---" % (time.time() - start_time))
