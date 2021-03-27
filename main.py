61d4d49667e7d2b8ef12d4da233bbb849f05b7e6
para5yte <tmitsu195@gmail.com>
2021-03-27 14:16:41 +1100

    change

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-# This is a sample Python script.
+# i made a change here
 
 # Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-# import cell
+
 import json
 import multiprocessing as mp
 import numpy as np
